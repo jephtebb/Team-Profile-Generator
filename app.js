dfd9d587c7aac315854d36260d81cb3e0de0a316
Jephtebb <jephteborno@yahoo.com>
2020-12-28 19:47:38 -0500

    Prompts set up for each team member

---
+++ app.js
@@ -0,0 +1,99 @@
+const fs = require ('fs');
+const inquirer = require('inquirer');
+const manager  = require('./lib/manager');
+const inter = require('./lib/inter');
+const engineer = require('./lib/engineer');
+const teamMemberArr = [];
+
+const members = () => {
+    const manager = () =>{
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: "What is the manager's name"
+        },
+        {
+                type: 'input',
+                name: 'managerIDnumber',
+                message: "What is the manager's ID number"
+        },
+        {
+                type: 'input',
+                name: 'managerEmail',
+                message: "What is the manager's email address"
+        },
+        {
+                type: 'input',
+                name: 'managerOfficeNumber',
+                message: "What is the manager's office number"
+        }
+
+    ]).then(response => {
+        const managerProfile = new manager(response.managerName, response.managerIDnumber,response.managerEmail,response.managerofficeNumber);
+        teamMemberArr.push(managerProfile);
+        addMember();
+
+       })
+    }
+    const engineer = () =>{
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: "What is the engineer's name"
+        },
+        {
+                type: 'input',
+                name: 'engineerIDnumber',
+                message: "What is the engineer's ID number"
+        },
+        {
+                type: 'input',
+                name: 'engineerEmail',
+                message: "What is the engineer's email address"
+        },
+        {
+                type: 'input',
+                name: 'githubUsername',
+                message: "What is the engineer's github username"
+        }
+
+    ]).then(response => {
+        const engineerProfile = new engineer(response.engineerName, response.engineerIDnumber,response.engineerEmail,response.githubUsername);
+        teamMemberArr.push(engineerProfile);
+        addMember();
+
+       })
+    }
+    const inter = () =>{
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: "What is the intern's name"
+        },
+        {
+                type: 'input',
+                name: 'internIDnumber',
+                message: "What is the intern's ID number"
+        },
+        {
+                type: 'input',
+                name: 'internEmail',
+                message: "What is the intern's email address"
+        },
+        {
+                type: 'input',
+                name: 'schoolAttended',
+                message: "Where does the intern go to school?"
+        }
+
+    ]).then(response => {
+        const internProfile = new intern(response.internName, response.internIDnumber,response.internEmail,response.schoolAttended);
+        teamMemberArr.push(internProfile);
+        addMember();
+
+       })
+    }
+}
